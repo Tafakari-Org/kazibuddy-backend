436c509b7f9e890b8e6a930feaa34c72612314cf
ritahchanger <peterdennis573@gmail.com>
2026-07-11 11:58:46 +0300

    fixed payment models

--- tafakari/payments/models.py
+++ tafakari/payments/models.py
@@ -81,6 +81,7 @@ class Transaction(models.Model):
     fees = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     processed_at = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return f"Transaction {self.id} - {self.status}"
+        
